368d3708428862caad62a8a01fb8f1ba03389ea1
Jesse Natalie <jenatali@microsoft.com>
2021-12-08 14:07:40 -0800

    Hack up the sample to draw a YUV texture

--- Chapter_9/Simple_Texture2D/Simple_Texture2D.c
+++ Chapter_9/Simple_Texture2D/Simple_Texture2D.c
@@ -36,7 +36,18 @@
 //    the basics of 2D texturing
 //
 #include <stdlib.h>
+#include <initguid.h>
+#include <d3d12.h>
 #include "esUtil.h"
+
+#pragma comment(lib, "d3d12.lib")
+
+PFNEGLCREATEIMAGEKHRPROC eglCreateImageKHR;
+PFNEGLDESTROYIMAGEKHRPROC eglDestroyImageKHR;
+PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES;
+
+ID3D12Device *d3d12device;
+ID3D12Resource *yuvTex;
 
 typedef struct
 {
@@ -46,46 +57,125 @@ typedef struct
    // Sampler location
    GLint samplerLoc;
 
-   // Texture handle
-   GLuint textureId;
+   EGLImageKHR image;
+   GLuint importedTextureId;
 
 } UserData;
 
 ///
 // Create a simple 2x2 texture image with four different colors
 //
-GLuint CreateSimpleTexture2D( )
+EGLImageKHR CreateSimpleTexture2D(ESContext *esContext)
 {
-   // Texture object handle
-   GLuint textureId;
+    ID3D12Debug *debug;
+    D3D12GetDebugInterface(&IID_ID3D12Debug, &debug);
+    debug->lpVtbl->EnableDebugLayer(debug);
 
-   // 2x2 Image, 3 bytes per pixel (R, G, B)
-   GLubyte pixels[4 * 3] =
-   {
-      255,   0,   0, // Red
-        0, 255,   0, // Green
-        0,   0, 255, // Blue
-      255, 255,   0  // Yellow
-   };
+    D3D12CreateDevice(NULL, D3D_FEATURE_LEVEL_11_0, &IID_ID3D12Device, &d3d12device);
 
-   // Use tightly packed data
-   glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
+    ID3D12CommandQueue *queue;
+    D3D12_COMMAND_QUEUE_DESC queue_desc = { D3D12_COMMAND_LIST_TYPE_DIRECT };
+    d3d12device->lpVtbl->CreateCommandQueue(d3d12device, &queue_desc, &IID_ID3D12CommandQueue, &queue);
 
-   // Generate a texture object
-   glGenTextures ( 1, &textureId );
+    ID3D12CommandAllocator *allocator;
+    d3d12device->lpVtbl->CreateCommandAllocator(d3d12device, D3D12_COMMAND_LIST_TYPE_DIRECT, &IID_ID3D12CommandAllocator, &allocator);
 
-   // Bind the texture object
-   glBindTexture ( GL_TEXTURE_2D, textureId );
+    ID3D12Fence *fence;
+    d3d12device->lpVtbl->CreateFence(d3d12device, 0, D3D12_FENCE_FLAG_NONE, &IID_ID3D12Fence, &fence);
 
-   // Load the texture
-   glTexImage2D ( GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels );
+    ID3D12GraphicsCommandList *list;
+    d3d12device->lpVtbl->CreateCommandList(d3d12device, 0, D3D12_COMMAND_LIST_TYPE_DIRECT, allocator, NULL, &IID_ID3D12GraphicsCommandList, &list);
 
-   // Set the filtering mode
-   glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-   glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    D3D12_RESOURCE_DESC nv12_desc = {
+        .Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D,
+        .Format = DXGI_FORMAT_NV12,
+        .Width = 4,
+        .Height = 4,
+        .DepthOrArraySize = 1,
+        .MipLevels = 1,
+        .Flags = D3D12_RESOURCE_FLAG_NONE,
+        .Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN,
+        .SampleDesc = {.Count = 1},
+    };
+    D3D12_PLACED_SUBRESOURCE_FOOTPRINT layouts[2];
+    UINT64 staging_size = 0;
+    d3d12device->lpVtbl->GetCopyableFootprints(d3d12device, &nv12_desc, 0, 2, 0, layouts, NULL, NULL, &staging_size);
 
-   return textureId;
+    D3D12_HEAP_PROPERTIES nv12_props = { .Type = D3D12_HEAP_TYPE_DEFAULT };
 
+    d3d12device->lpVtbl->CreateCommittedResource(d3d12device, &nv12_props, D3D12_HEAP_FLAG_SHARED, &nv12_desc, D3D12_RESOURCE_STATE_COPY_DEST, NULL, &IID_ID3D12Resource, &yuvTex);
+
+    ID3D12Resource *staging;
+    D3D12_RESOURCE_DESC staging_desc = {
+        .Dimension = D3D12_RESOURCE_DIMENSION_BUFFER,
+        .Format = DXGI_FORMAT_UNKNOWN,
+        .Width = staging_size,
+        .Height = 1,
+        .DepthOrArraySize = 1,
+        .MipLevels = 1,
+        .Flags = D3D12_RESOURCE_FLAG_NONE,
+        .Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+        .SampleDesc = {.Count = 1 },
+    };
+    D3D12_HEAP_PROPERTIES staging_props = { .Type = D3D12_HEAP_TYPE_UPLOAD };
+    d3d12device->lpVtbl->CreateCommittedResource(d3d12device, &staging_props, D3D12_HEAP_FLAG_NONE, &staging_desc, D3D12_RESOURCE_STATE_COMMON, NULL, &IID_ID3D12Resource, &staging);
+
+    void *texelData;
+    staging->lpVtbl->Map(staging, 0, NULL, &texelData);
+
+    UINT8 *ys = (UINT *)texelData;
+    UINT8 *uvs = ys + layouts[1].Offset;
+    for (unsigned y = 0; y < layouts[0].Footprint.Height; ++y) {
+        UINT8 *ys_row = ys + layouts[0].Footprint.RowPitch * y;
+        for (unsigned x = 0; x < layouts[0].Footprint.Width; ++x) {
+            ys_row[x] = y * x * 8;
+        }
+    }
+    for (unsigned y = 0; y < layouts[1].Footprint.Height; ++y) {
+        UINT8 *uvs_row = uvs + layouts[1].Footprint.RowPitch * y;
+        for (unsigned x = 0; x < layouts[1].Footprint.Width; ++x) {
+            uvs_row[x * 2] = y * 255;
+            uvs_row[x * 2 + 1] = x * 255;
+        }
+    }
+
+    staging->lpVtbl->Unmap(staging, 0, NULL);
+
+    for (unsigned i = 0; i < 2; ++i) {
+        D3D12_TEXTURE_COPY_LOCATION copy_src = {
+            .Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT,
+            .PlacedFootprint = layouts[i],
+            .pResource = staging
+        };
+        D3D12_TEXTURE_COPY_LOCATION copy_dest = {
+            .Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX,
+            .SubresourceIndex = i,
+            .pResource = yuvTex
+        };
+
+        list->lpVtbl->CopyTextureRegion(list, &copy_dest, 0, 0, 0, &copy_src, NULL);
+    }
+
+    D3D12_RESOURCE_BARRIER barrier = { .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+        .Transition = {
+            .pResource = yuvTex,
+            .StateBefore = D3D12_RESOURCE_STATE_COPY_DEST,
+            .StateAfter = D3D12_RESOURCE_STATE_COMMON,
+            .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+        }
+    };
+    list->lpVtbl->ResourceBarrier(list, 1, &barrier);
+
+    list->lpVtbl->Close(list);
+    queue->lpVtbl->ExecuteCommandLists(queue, 1, &list);
+    queue->lpVtbl->Signal(queue, fence, 1);
+    fence->lpVtbl->SetEventOnCompletion(fence, 1, NULL);
+
+    HANDLE shared_handle = NULL;
+    d3d12device->lpVtbl->CreateSharedHandle(d3d12device, yuvTex, NULL, GENERIC_ALL, NULL, &shared_handle);
+
+    EGLint attrib[] = { EGL_NONE };
+    return eglCreateImageKHR(esContext->eglDisplay, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, shared_handle, attrib);
 }
 
 
@@ -108,10 +198,11 @@ int Init ( ESContext *esContext )
 
    char fShaderStr[] =
       "#version 300 es                                     \n"
+      "#extension GL_OES_EGL_image_external_essl3 : require\n"
       "precision mediump float;                            \n"
       "in vec2 v_texCoord;                                 \n"
       "layout(location = 0) out vec4 outColor;             \n"
-      "uniform sampler2D s_texture;                        \n"
+      "uniform samplerExternalOES s_texture;               \n"
       "void main()                                         \n"
       "{                                                   \n"
       "  outColor = texture( s_texture, v_texCoord );      \n"
@@ -123,8 +214,16 @@ int Init ( ESContext *esContext )
    // Get the sampler location
    userData->samplerLoc = glGetUniformLocation ( userData->programObject, "s_texture" );
 
+   eglCreateImageKHR = eglGetProcAddress("eglCreateImageKHR");
+   eglDestroyImageKHR = eglGetProcAddress("eglDestroyImageKHR");
+   glEGLImageTargetTexture2DOES = eglGetProcAddress("glEGLImageTargetTexture2DOES");
+
    // Load the texture
-   userData->textureId = CreateSimpleTexture2D ();
+   userData->image = CreateSimpleTexture2D (esContext);
+
+   glGenTextures(1, &userData->importedTextureId);
+   glBindTexture(GL_TEXTURE_EXTERNAL_OES, userData->importedTextureId);
+   glEGLImageTargetTexture2DOES(GL_TEXTURE_EXTERNAL_OES, userData->image);
 
    glClearColor ( 1.0f, 1.0f, 1.0f, 0.0f );
    return TRUE;
@@ -168,7 +267,7 @@ void Draw ( ESContext *esContext )
 
    // Bind the texture
    glActiveTexture ( GL_TEXTURE0 );
-   glBindTexture ( GL_TEXTURE_2D, userData->textureId );
+   //glBindTexture ( GL_TEXTURE_2D, userData->textureId );
 
    // Set the sampler texture unit to 0
    glUniform1i ( userData->samplerLoc, 0 );
@@ -184,7 +283,8 @@ void ShutDown ( ESContext *esContext )
    UserData *userData = esContext->userData;
 
    // Delete texture object
-   glDeleteTextures ( 1, &userData->textureId );
+   glDeleteTextures ( 1, &userData->importedTextureId );
+   eglDestroyImageKHR(esContext->eglDisplay, userData->image);
 
    // Delete program object
    glDeleteProgram ( userData->programObject );
